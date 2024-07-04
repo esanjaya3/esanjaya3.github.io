ff54d67aebd5d0a354085d19f3a24fc376bde738
Edwin Sanjaya <54498646+esanjaya3@users.noreply.github.com>
2024-07-04 09:19:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function moveRandomEl(elm) {
   elm.style.position = "absolute";
-  elm.style.top = Math.floor(Math.random() * 70 + 5) + "%";
+  elm.style.top = Math.floor(Math.random() * 90 + 5) + "%";
+  elm.style.left = Math.floor(Math.random() * 90 + 5) + "%";
 }
 
 const moveRandom = document.querySelector("#move-random");
